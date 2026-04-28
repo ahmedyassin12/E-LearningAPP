5c324b562530f41e4be0caaba4cf3ab2039056f1
ahmedyassin12 <ahmedyassinzouaoui@gmail.com>
2026-04-28 15:59:02 +0200

    adjust app-prod.properties

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -32,7 +32,7 @@ public class TestApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:9090"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
